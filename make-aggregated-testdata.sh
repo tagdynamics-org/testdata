374fbec1c1c9358e888c41dc4bc29ae4060cc028
Matias Dahl <matias.dahl@iki.fi>
2018-08-02 21:06:09 +0300

    updated test data without delta=0 filtering

--- make-aggregated-testdata.sh
+++ make-aggregated-testdata.sh
@@ -1,18 +1,16 @@
 set -eux
 
+# make-aggregated-testdata.sh /path/to/input.jsonl /path/to/output/dir/
+
 TESTDATA_REPO=`pwd`
 INPUT_DATA=$TESTDATA_REPO/2-extracted-osm-metadata.jsonl
+OUTPUT_DIR=$TESTDATA_REPO/3-aggregates/
 
-# Will fail if directory exists. 
-mkdir $TESTDATA_REPO/3-aggregates
+# Will fail if directory exists.
+mkdir $OUTPUT_DIR
 
-git clone git@github.com:tagdynamics-org/osm-tag-aggregator.git
+git clone --recurse-submodules git@github.com:tagdynamics-org/osm-tag-aggregator.git
 cd osm-tag-aggregator
-
 gradle wrapper
-
-bash launch.sh LIVE_REVCOUNTS        $INPUT_DATA $TESTDATA_REPO/3-aggregates/live-revcounts.jsonl
-bash launch.sh TOTAL_REVCOUNTS       $INPUT_DATA $TESTDATA_REPO/3-aggregates/total-revcounts.jsonl
-bash launch.sh TRANSITION_COUNTS     $INPUT_DATA $TESTDATA_REPO/3-aggregates/transition-counts.jsonl
-bash launch.sh PER_DAY_DELTA_COUNTS  $INPUT_DATA $TESTDATA_REPO/3-aggregates/per-day-delta-counts.jsonl
+bash compute-all.sh $INPUT_DATA $OUTPUT_DIR
 
